2eaecff785b2fd095b244c84c1f734d90ad7e89f
Umut Topuzoğlu <uloco@schiefewelt.de>
2020-10-29 09:46:41 +0100

    Add overall theme

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -3,16 +3,16 @@ import { BlulocoDark, BlulocoLight } from "../theme/themes";
 
 export const theme = /*#__PURE__*/ createTheme({
   light: {
-    // background: "#f0f0ea",
-    // text: "#383a42",
+    background: "#f0f0ea",
+    text: "#383a42",
     primary: "#0099e1",
     primaryContrast: "#f0f0ea",
     border: "#d5d7d8",
     code: "#7a82da",
   },
   dark: {
-    // background: "#22252a",
-    // text: "#abb2bf",
+    background: "#22252a",
+    text: "#abb2bf",
     primary: "#2f8eff",
     primaryContrast: "#22252a",
     border: "#3d434f",
@@ -22,5 +22,27 @@ export const theme = /*#__PURE__*/ createTheme({
     wmbar: false,
     dark: BlulocoDark,
     light: BlulocoLight,
+  },
+  quote: {
+    light: {
+      background: "#0000000a",
+      border: "#d5d7d8",
+      text: "#383a4290",
+    },
+    dark: {
+      background: "#ffffff09",
+      border: "#3d434f",
+      text: "#abb2bf90",
+    },
+  },
+  toc: {
+    light: {
+      background: "#f0f0ea0a",
+      border: "#d5d7d8",
+    },
+    dark: {
+      background: "#22252a0a",
+      border: "#3d434f",
+    },
   },
 });
